4886c504262b03849205f3eca2f9a131face9d40
Kristin <kgalea13@gmail.com>
2026-07-28 22:32:32 -0400

    Added constant variables, and corrected main loop

--- Project_3.py
+++ Project_3.py
@@ -1,8 +1,23 @@
+
+TIER_B_MIN = 9
+TIER_B_MAX = 15
+TIER_M_MIN = 14
+TIER_M_MAX = 20
+TIER_P_MIN = 20
+TIER_P_MAX = 25
+
+TIER_B_MIN_COMM = 50
+TIER_B_MAX_COMM = 75
+
+TIER_M_MIN_COMM = 60
+TIER_M_MAX_COMM = 100
+
+TIER_P_MIN_COMM = 75
+TIER_P_MAX_COMM = 125
+
 run_again = 'y'.upper()
 
 while run_again == 'y'.upper():
-
-    
 
 
     employee_name = input('Enter Employee\'s name: ' )
@@ -22,17 +37,17 @@ while run_again == 'y'.upper():
     
 
     if tier == 'B':
-        if item_sold > 9 and item_sold <= 15:
+        if item_sold > TIER_B_MIN and item_sold <= TIER_B_MAX:
             commission_items = item_sold - 9
-            calculate_commission = commission_items * 50
+            calculate_commission = commission_items * TIER_B_MIN_COMM 
             total_salary = base_salary + calculate_commission
             print(total_salary)
 
-        elif item_sold > 15:
+        elif item_sold > TIER_B_MAX:
             commission_items_50 = 6
-            calculate_commission_50 = commission_items_50 * 50
+            calculate_commission_50 = commission_items_50 * TIER_B_MIN_COMM 
             commission_items_75 = item_sold - 15
-            calculate_commission_75 = commission_items_75 * 75
+            calculate_commission_75 = commission_items_75 * TIER_B_MAX_COMM 
             total_salary = base_salary + calculate_commission_50 + calculate_commission_75 
             print(total_salary)
 
@@ -40,17 +55,17 @@ while run_again == 'y'.upper():
             print('WARNING: Sales must improve.')
 
     if tier == 'M':
-        if item_sold > 14 and item_sold <= 20:
+        if item_sold > TIER_M_MIN and item_sold <= TIER_M_MAX:
             commission_items = item_sold - 14
-            calculate_commission = commission_items * 60
+            calculate_commission = commission_items * TIER_M_MIN_COMM
             total_salary = base_salary + calculate_commission
             print(total_salary)
 
-        elif item_sold > 20:
+        elif item_sold > TIER_M_MAX:
             commission_items_60 = 6
-            calculate_commission_60 = commission_items_60 * 60
+            calculate_commission_60 = commission_items_60 * TIER_M_MIN_COMM
             calculate_itmes_100 = item_sold - 20
-            calculate_commission_100 = calculate_itmes_100 * 100
+            calculate_commission_100 = calculate_itmes_100 * TIER_M_MAX_COMM
             total_salary = calculate_commission_100 + base_salary
             print(total_salary)
 
@@ -59,25 +74,24 @@ while run_again == 'y'.upper():
 
 
     if tier == 'P' or tier == 'p':
-        if item_sold >= 20 and item_sold <= 25:
+        if item_sold >= TIER_P_MIN  and item_sold <= TIER_P_MAX:
             commission_items = 6
-            calculate_commission = commission_items * 75
+            calculate_commission = commission_items * TIER_P_MIN_COMM
             total_salary = calculate_commission + base_salary
             print(total_salary)
 
-        elif item_sold > 25:
+        elif item_sold > TIER_P_MAX:
             commission_items_75 = 6
-            calculate_commission_75 = commission_items_75 * 75
+            calculate_commission_75 = commission_items_75 * TIER_P_MIN_COMM
             commission_items_125 = item_sold - 25
-            calculate_commission_125 = commission_items_125 * 125
+            calculate_commission_125 = commission_items_125 * TIER_P_MAX_COMM
             total_salary = base_salary + calculate_commission_75 + calculate_commission_125
             print(total_salary)
 
         else:
             print('WARNING: Sales must improve to stay in Tier P.')
 
-
-    run_again = input('Do you want to enter another employee? y/n: ')
+    run_again = input('Do you want to enter another employee? y/n: ')[0].upper()
 
     
     
